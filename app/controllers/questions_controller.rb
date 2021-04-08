b2e5bef7cc9ef47f1eb3b283d0ac43fd2a336268
Lorena Marinho <lor.marinho@gmail.com>
2021-04-08 20:00:18 -0300

    working app, styling to go

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,17 @@ class QuestionsController < ApplicationController
     end
 
     def answer
-        @answer
+        @answer = params[:question]
+        @coach_answer = question(@answer)
+    end
+
+    def question(answer)
+        if @answer.downcase == "i am going to work right now!"
+            return "You had better!"
+        elsif @answer.include? "?"
+            return "Silly question, get dressed and go to work!"
+         else
+            return "I don't care, get dressed and go to work!"
+         end
     end
 end
